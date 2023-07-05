64b016e05953fe63911de804a50a4ab7a5f1d6fb
KlopovaMM <is24klopovamm@artcollege.ru>
2023-07-05 09:59:12 +0300

    Создание нового проекта AppKlopova

--- ClassLibKlopova/College.cs
+++ ClassLibKlopova/College.cs
@@ -1,4 +1,12 @@
-﻿namespace ClassLibKlopova
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ClassLibKlopova
 {
     public class College//основной класс
     {
